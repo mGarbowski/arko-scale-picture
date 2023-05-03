88cc38b450b746e90ac7e4ade02decd9f4d5675e
Mikołaj Garbowski <mikolaj.garbowski@gmail.com>
2023-05-03 13:57:42 +0200

    Reading bitmap size from memory

--- scalePicture.asm
+++ scalePicture.asm
@@ -19,39 +19,63 @@ main:
 	li	a7, 1024
 	ecall			# open file in read-only mode
 	
-	mv	t0, a0		# copy file descriptor
-	la	a1, buffer
-	li	a2, 512
-	li	a7, 63
-	ecall
-	
-	la	t1, buffer
-	lb 	a0, 0x22(t1)	# load size
-	lb	t3, 0x23(t1)
-	slli	t3, t3, 8	# shifting and adding because it's unaligned, little endian
-	add	a0, a0, t3
-	lb	t3, 0x24(t1)
-	slli	t3, t3, 16
-	add	a0, a0, t3
-	lb	t3, 0x25(t1)
-	slli	t3, t3, 24
-	mv	t2, a0		# copy size
-	add	a0, a0, t3	# size of bitmap in bytes  is in a0
-	li	a7, 9
-	ecall			# allocate memory for bitmap on the heap
-	mv	t3, a0		# copy pointer
-	
-	mv	a0, t0		# file descriptor
-	li	a1, 0x36	# offset, where bitmap data begins
-	li	a2, 0		# idk??
-	li	a7, 62
-	ecall			# seek file dscriptor to bitmap data
-	
-	mv	a0, t0		# file descriptor
-	mv	a1, t3		# pointer to bitmap buffer
-	mv	a2, t2		# length to read
-	ecall 			# read all bitmap data
-	
+	mv	s1, a0		# copy file descriptor
+	call 	readBitmapSize
 	
 	li	a7, 10
 	ecall			# Exit
+
+
+# Allocate space for the bitmap on the heap and load bitmap data
+# Arguments
+# a0 - file descriptor of an open .bmp file
+# Returns
+# a0 - pointer to bitmap data
+loadFileToMemory:
+	ret
+	
+	
+# Read size of the bitmap from .bmp file header
+# Arguments
+# a0 - file descriptor of an open .bmp file
+# Returns
+# a0 - size of the bitmap (4B)
+readBitmapSize:
+	addi	sp, sp, -4	# Save return address on the stack
+	sw	ra, (sp)
+	
+	la	a1, buffer
+	li	a2, 512
+	li	a7, 63
+	ecall			# Read beginning of the file into buffer
+	
+	la	a0, buffer
+	addi	a0, a0, 0x22	# Offset of bitmap size field in the header
+	call 	read4BytesLE
+	
+	lw	ra, (sp)
+	addi	sp, sp, 4	# Restore return address from the stack
+	ret
+	
+
+# Read 4 consecutive bytes from memory representing integer in little endian
+# Arguments
+# a0 - address of the first byte
+# Returns
+# a0 - 4B signed integer 
+read4BytesLE:
+	lb	t0, (a0)
+	lb	t2, 1(a0)
+	slli	t2, t2, 8
+	add	t0, t0, t2
+	lb	t2, 2(a0)
+	slli	t2, t2, 16
+	add	t0, t0, t2
+	lb	t2, 3(a0)
+	slli	t2, t2, 24
+	add	t0, t0, t2
+	mv	a0, t0
+	ret
+
+
+
